b2344efe2c61fbf17f0f0f007ab081fe18036c9b
nmenashe <nmenashe@medallia.com>
2020-03-06 22:33:12 +0200

    - alarm methds created

--- index.android.js
+++ index.android.js
@@ -4,7 +4,21 @@ import { NativeModules } from 'react-native'
 
 export default class AppWaker {
 
-    static doIt(){
-        NativeModules.AppWaker.doIt();
+    static setAlarm(id, timestamp, inexact){
+        if(!(typeof id === 'string' || typeof id === 'number'))
+            throw new Error('AppWaker.setAlarm: `id` must be a number or a string.')
+        if(typeof timestamp !== 'number')
+            throw new Error('AppWaker.setAlarm: `timestamp` must be an integer.')
+        id = id.toString()  // parse to string
+        timestamp = Math.trunc(timestamp)   // parse to int
+        inexact = !!inexact // parse to bool
+        NativeModules.AppWaker.setAlarm(id, timestamp, inexact)
+    }
+
+    static clearAlarm(id) {
+        if(!(typeof id === 'string' || typeof id === 'number'))
+            throw new Error('AppWaker.clearAlarm: `id` must be a number or a string.')
+        id = id.toString()  // parse to string
+        NativeModules.AppWaker.clearAlarm(id)
     }
 }
