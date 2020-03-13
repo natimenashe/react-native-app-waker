fc79c7e07fdf6176fcb2d086bcf27276e5f00df7
nmenashe <nmenashe@medallia.com>
2020-03-13 21:01:30 +0200

    - work manager integrated

--- app/src/main/java/com/idom/appWaker/AlarmReceiver.java
+++ app/src/main/java/com/idom/appWaker/AlarmReceiver.java
@@ -13,13 +13,13 @@ public class AlarmReceiver extends BroadcastReceiver {
     }
 
     private void launchApplication(Context context, String alarmID) {
-        String packageName = context.getApplicationContext().getPackageName();
-        Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
-
-        launchIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        launchIntent.putExtra("alarmID", alarmID);
-
-        context.startActivity(launchIntent);
-        Log.i("ReactNativeAppWaker", "AlarmReceiver: Launching: " + packageName);
+//        String packageName = context.getApplicationContext().getPackageName();
+//        Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+//
+//        launchIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        launchIntent.putExtra("alarmID", alarmID);
+//
+//        context.startActivity(launchIntent);
+//        Log.i("ReactNativeAppWaker", "AlarmReceiver: Launching: " + packageName);
     }
 }
