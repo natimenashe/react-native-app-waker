c1d1526fbeabc8860fc42ce31f956610b4bbdbc7
nmenashe <nmenashe@medallia.com>
2020-05-09 00:53:38 +0300

    minor

--- app/src/main/java/com/idom/appWaker/WakerModule.java
+++ app/src/main/java/com/idom/appWaker/WakerModule.java
@@ -2,14 +2,10 @@ package com.idom.appWaker;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.app.job.JobInfo;
-import android.app.job.JobScheduler;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
-import android.os.PersistableBundle;
 import android.provider.Settings;
 import android.util.Log;
 import androidx.work.Data;
